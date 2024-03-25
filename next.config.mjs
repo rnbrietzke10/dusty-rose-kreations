228f09b65ea7697d85dabf14ec53c530a8c6a439
Rebecca Brietzke <rnbrietzke@outlook.com>
2024-03-25 11:53:44 -0500

    add environment variables, and mongoose, and  mongoose-unique-validator packages for MongoDB

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
+const config = require('./config');
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  DB_URI: config.DB_URI,
+};
 
 export default nextConfig;
